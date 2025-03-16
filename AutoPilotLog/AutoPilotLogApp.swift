5e7f619014081be869b81171e8548868d51aa539
Suho Park <psooh1219@icloud.com>
2025-03-16 09:46:12 +0900

    Initial Commit

---
+++ AutoPilotLog/AutoPilotLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPilotLogApp.swift
+//  AutoPilotLog
+//
+//  Created by Suho Park on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoPilotLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoPilotLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoPilotLog
+//
+//  Created by Suho Park on 3/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
